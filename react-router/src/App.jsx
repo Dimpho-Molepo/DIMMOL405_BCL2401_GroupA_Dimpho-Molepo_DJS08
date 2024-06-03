5a33d921e1aeb70f7c4be0dab1ed40d59b94db46
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-06-03 19:18:53 +0200

    Update the absolute paths to relative paths

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -18,14 +18,15 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
-
-          <Route path="/host" element={<HostLayout />}>
-            <Route path="/host/income" element={<Income />} />
-            <Route path="/host/reviews" element={<Reviews />} />
+        <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          
+          <Route path="host" element={<HostLayout />}>
+            <Route path="host" element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
 
         </Route>
