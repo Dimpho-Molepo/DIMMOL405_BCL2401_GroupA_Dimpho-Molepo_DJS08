c7337f5c18a70d060ba86c2b4685e861fcf4215f
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-06-05 20:54:27 +0200

    Refactor getHostVans function

--- react-router/src/api.js
+++ react-router/src/api.js
@@ -16,40 +16,24 @@ const db = getFirestore(app)
 // Refactoring the fetching functions below
 const vansCollectionRef = collection(db, "vans")
 
-export async function getVans() {
-    const snapshot = await getDocs(vansCollectionRef)
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
     const vans = snapshot.docs.map(doc => ({
         ...doc.data(),
         id: doc.id
     }))
-
     return vans
 }
-
 export async function getVan(id) {
     const docRef = doc(db, "vans", id) 
     const snapshot = await getDoc(docRef)
     return {
         ...snapshot.data(),
         id: snapshot.id
-    }
+    } 
 }
 
-
-
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
-}
 
 export async function loginUser(creds) {
     const res = await fetch("/api/login",
